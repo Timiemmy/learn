ac0d33279028d158ab69c8a2f773ac84ac2e83b3
Timiemmy <faith.bolanle@yahoo.com>
2023-04-19 14:42:47 +0100

    added permissions and mixins in courses views

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,65 @@
 from django.shortcuts import render
+from .models import Course
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class ManageCourseListView(ListView):
+    model = Course
+    template_name = 'courses/manage/course/list.html'
+
+# override the get_queryset() method of the view to retrieve only courses created by the current user.
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.filter)
+
+
+# This class can be used for views that interact with any model that contains an owner attribute.
+class OwnerMixin:
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.filter)
+
+
+class OwnerEditMixin:
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin, PermissionRequiredMixin):
+    model = Course
+    # The fields of the model to build the model form of the CreateView and UpdateView views.
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')# Used by CreateView, UpdateView, and DeleteView to redirect the user after the form is successfully submitted or the object is deleted.
+
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    # The template you will use for the CreateView and UpdateView views.
+    template_name = 'courses/manage/course/form.html'
+
+
+class ManageCourseListView(OwnerCourseMixin, ListView):
+    #Lists the courses created by the user. It inherits from OwnerCourseMixin and ListView.
+    template_name = 'courses/manage/course/list.html'
+    permission_required = 'courses.view_course'
+
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    # Uses a model form to create a new Course object. It uses the fields defined in OwnerCourseMixin to build a model form and also subclasses CreateView
+    permission_required = 'courses.add_courses'
+
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    '''Allows the editing of an existing Course object. It uses the fields defined in
+OwnerCourseMixin to build a model form and also subclasses UpdateView. It uses the template
+defined in OwnerCourseEditMixin.'''
+    permission_required = 'courses.change_course'
+
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    # Inherits from OwnerCourseMixin and the generic DeleteView.
+    template_name = 'courses/manage/course/delete.html'
+    permission_required = 'courses.delete_course'
